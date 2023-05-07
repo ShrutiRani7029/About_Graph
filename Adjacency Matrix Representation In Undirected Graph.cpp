e9e9ff4061bd561b5eca34885b3e3bda647aac8d
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-05-08 02:57:08 +0530

    Create Adjacency Matrix Representation In Undirected Graph.cpp
    N<=10^3, N!=10^5
    TIME COMPLEXITY: O(N^2)
    SPACE COMPLEXITY: O(N^2)

---
+++ Adjacency Matrix Representation In Undirected Graph.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3 +10;
+int graph[N][N];
+int main(){
+
+int n,m;
+cin>>n>>m;
+for(int i=0;i<m;i++)
+{
+    int v1,v2;
+    cin>>v1>>v2;
+    graph[v1][v2]=1;///v1 , v2 are connected
+    graph[v2][v1]=1;///v2, v1 are connected
+}
+for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<graph[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+
+}
