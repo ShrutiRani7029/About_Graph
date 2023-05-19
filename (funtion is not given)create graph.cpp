43b6c4210d16c12c8775c08b03bed53a432f7427
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-05-19 20:10:05 +0530

    Create (funtion is not given)create graph.cpp
    complexity: o(n+m)

---
+++ (funtion is not given)create graph.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+vector<int> graph[N];
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int v1, v2;
+        cin >> v1 >> v2;
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << i << " -> ";
+        for (int j = 0; j < graph[i].size(); j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
